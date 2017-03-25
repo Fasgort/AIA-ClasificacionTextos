0e7759ee47285fb3b8ede03c1d5f866490ea367d
David López Chica <Fasgort@gmail.com>
2017-03-25 21:05:37 +0100

    Implementada la vectorización de textos.

--- vectorizacionTextos.py
+++ vectorizacionTextos.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.sparse import coo_matrix
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import pairwise_distances
 import unicodedata
+import operator
 from nltk import downloader
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 downloader.download("stopwords")
+np.set_printoptions(threshold=np.nan)
 
 stopwords_list = set(stopwords.words("spanish"))
 stemmer = SnowballStemmer("spanish")
-
-documentos = []
-documentos.append("oro plata camión") # Esta es la consulta
-documentos.append("Éste texto no tiene nada que ver con los demás")
-documentos.append("La plata fue entregada en camiones color plata")
-documentos.append("El cargamento de oro llegó en un camión. El cargamento de oro llegó en un camión. El cargamento de oro llegó en un camión")
-documentos.append("Cargamentos de oro dañados por el fuego")
-documentos.append("El cargamento de oro llegó en un camión")
 
 def tratamiento1(documentos):
     # Tratamiento de datos básico
@@ -50,3 +45,97 @@ def tratamiento3(documentos):
         new_documentos.append(" ".join(stemmed_words))
     return new_documentos
 
+# Main
+
+documentosOrig = []
+documentosOrig.append("oro plata camión") # Esta es la consulta
+documentosOrig.append("Éste texto no tiene nada que ver con los demás")
+documentosOrig.append("La plata fue entregada en camiones color plata")
+documentosOrig.append("El cargamento de oro llegó en un camión. El cargamento de oro llegó en un camión. El cargamento de oro llegó en un camión")
+documentosOrig.append("Cargamentos de oro dañados por el fuego")
+documentosOrig.append("El cargamento de oro llegó en un camión")
+
+print()
+print("////////////////////////////////")
+print("///////////RESULTADOS///////////")
+print("////////////////////////////////")
+print()
+print("Menor distancia equivale a mayor relevancia en la consulta.")
+print()
+
+###
+### EJERCICIO 1
+###
+documentos = tratamiento1(documentosOrig)
+countVectorizer = CountVectorizer()
+documentosVect = countVectorizer.fit_transform(documentos[1::]) # Ignoramos la consulta en la generación del vectorizador
+consultaVect = countVectorizer.transform(documentos[:1:]) # Generamos el vector de la consulta
+                                        
+print("Con el modelo vectorial simple, y la distancia euclídea")
+relevanciaDocEj1 = []
+for d in range(documentosVect.shape[0]):
+    relevanciaDocEj1.append((d+1,pairwise_distances(documentosVect.getrow(d), consultaVect, metric='euclidean')[0][0]))
+relevanciaDocEj1.sort(key=operator.itemgetter(1))
+print(relevanciaDocEj1)
+print()
+
+###
+### EJERCICIO 2
+### Comentario: La distancia devuelta es 1 - similitud coseno
+### por lo que los resultados más relevantes son los que se acercan a 0.
+###
+print("Con el modelo vectorial simple, y la distancia normalizada (coseno)")
+relevanciaDocEj2 = []
+for d in range(documentosVect.shape[0]):
+    relevanciaDocEj2.append((d+1,pairwise_distances(documentosVect.getrow(d), consultaVect, metric='cosine')[0][0]))
+relevanciaDocEj2.sort(key=operator.itemgetter(1))
+print(relevanciaDocEj2)
+print()
+
+###
+### EJERCICIO 3
+###
+documentos = tratamiento2(documentosOrig)
+countVectorizer = CountVectorizer()
+documentosVect = countVectorizer.fit_transform(documentos[1::]) # Ignoramos la consulta en la generación del vectorizador
+consultaVect = countVectorizer.transform(documentos[:1:]) # Generamos el vector de la consulta
+
+print("Con el modelo vectorial simple, la distancia normalizada (coseno) y empleando stopwords")
+relevanciaDocEj3 = []
+for d in range(documentosVect.shape[0]):
+    relevanciaDocEj3.append((d+1,pairwise_distances(documentosVect.getrow(d), consultaVect, metric='cosine')[0][0]))
+relevanciaDocEj3.sort(key=operator.itemgetter(1))
+print(relevanciaDocEj3)
+print()
+
+###
+### EJERCICIO 4
+###
+documentos = tratamiento3(documentosOrig)
+countVectorizer = CountVectorizer()
+documentosVect = countVectorizer.fit_transform(documentos[1::]) # Ignoramos la consulta en la generación del vectorizador
+consultaVect = countVectorizer.transform(documentos[:1:]) # Generamos el vector de la consulta
+
+print("Con el modelo vectorial simple, la distancia normalizada (coseno), empleando stopwords y stemming")
+relevanciaDocEj4 = []
+for d in range(documentosVect.shape[0]):
+    relevanciaDocEj4.append((d+1,pairwise_distances(documentosVect.getrow(d), consultaVect, metric='cosine')[0][0]))
+relevanciaDocEj4.sort(key=operator.itemgetter(1))
+print(relevanciaDocEj4)
+print()
+
+###
+### EJERCICIO 5
+###
+documentos = tratamiento3(documentosOrig)
+tfidVectorizer = TfidfVectorizer()
+documentosVect = tfidVectorizer.fit_transform(documentos[1::]) # Ignoramos la consulta en la generación del vectorizador
+consultaVect = tfidVectorizer.transform(documentos[:1:]) # Generamos el vector de la consulta
+
+print("Con el modelo de frecuencia documental inversa, la distancia normalizada (coseno), empleando stopwords y stemming")
+relevanciaDocEj5 = []
+for d in range(documentosVect.shape[0]):
+    relevanciaDocEj5.append((d+1,pairwise_distances(documentosVect.getrow(d), consultaVect, metric='cosine')[0][0]))
+relevanciaDocEj5.sort(key=operator.itemgetter(1))
+print(relevanciaDocEj5)
+print()
