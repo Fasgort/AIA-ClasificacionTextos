b081ca1e87ed7f10febca79ce993b8c9fb024cf0
David López Chica <Fasgort@gmail.com>
2017-04-01 22:36:42 +0200

    Implementada la carga del corpus, descartando aquellos tweets que no se encuentran incluídos, o no están escritos en inglés.

--- analisisSentimientos.py
+++ analisisSentimientos.py
@@ -7,6 +7,15 @@ tweets = [] # Subject, classification, tweetId
 f = open('AnalisisSentimientos\\corpus.csv', 'r')
 for line in f:
     line = re.sub('["\\n]', '', line)
-    tweets.append(line.split(","))
+    tweet = line.split(",")
+    try:
+        jsonf = open('AnalisisSentimientos\\rawdata\\' + str(tweet[2]) + '.json' )
+        json_data = json.load(jsonf)
+        if json_data["lang"] != "en":
+            continue
+        tweet[2] = json_data["text"]
+    except:
+        continue
+    tweets.append(tweet)
 
 
